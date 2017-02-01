b6ff01fcc677bd6be6690f7cda56f55f08baab97
Rey Meljohn S. Guieb <reymeljohnguieb22@gmail.com>
2017-02-01 10:33:46 +0800

    Added files

--- Exercise2/src/exercise2/Exercise2.java
+++ Exercise2/src/exercise2/Exercise2.java
@@ -1,30 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package exercise2;
 
-import java.net.*;
-import java.lang.*;
-import java.io.*;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
- * This program checks ports 1 to 1024 that have connection.
- * 
- * @author Rey Meljohn S. Guieb
+ *
+ * @author s326lab
  */
-public class Exercise2{
+public class NewClass {
+    
     public static void main(String[] args) {
         Scanner kbd = new Scanner(System.in);
         InetAddress ia;
-        System.out.print("Enter host: ");
+        System.out.print("Enter host[: ");
         String input = kbd.nextLine();
-        
-        for(int x = 1; x <=1024; x++){
+        for (int x = 1; x <= 1024; x++) {
             try {
                 Socket sock = new Socket(input, x);
-                System.out.println(":" +x +" ---Connection open!");
-            } catch (IOException ex) {   
+                System.out.println(":" + x + " ---Connection open!");
+            } catch (IOException ex) {
             }
         }
-    }   
+    }
+    
 }
